a5f1357c2960e9c07109c466a289fd217312b4e6
Fabian Weik <fabian.weik@protonmail.com>
2020-10-14 00:27:16 +0200

    data | use data objects instead of dictionaries

--- Experiments/Data/acn_data.py
+++ Experiments/Data/acn_data.py
@@ -1,4 +1,5 @@
 import json
+from typing import List
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
@@ -8,28 +9,29 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from Experiments.Data.demand_data import DemandData
 
-class ACN_Data(object):
-  unimportant_keys = ['clusterID', 'siteID', 'spaceID', 'stationID', 'timezone', 'userID', 'userInputs']
-  date_keys = ['connectionTime', 'disconnectTime', 'doneChargingTime']
+class ACN_DataItem(object):
+  start: datetime
+  end: datetime
+  kWh: float
+  period_hours: float
+  kw: float
 
+class ACN_Data(object):
+  date_keys = ['connectionTime', 'disconnectTime', 'doneChargingTime']
   dateformat ='%a, %d %b %Y %H:%M:%S %Z'
 
   def __init__(self, data_file_name) -> None:
     self.read_data(data_file_name)
+    self.items: list[ACN_DataItem] = []
 
   def read_data(self, data_file_name) -> None:
     with open(data_file_name, 'r') as file:
       raw_data = json.load(file)
 
-    self.items = raw_data['_items']
-
-  def remove_unnescessary_info(self) -> None:
-    for item in self.items:
-      for key in self.unimportant_keys:
-        item.pop(key, None)
+    self.raw_items = raw_data['_items']
 
   def parse_dates(self) -> None:
-    for item in self.items:
+    for item in self.raw_items:
       for key in self.date_keys:
         datestr = item[key]
 
@@ -37,25 +39,33 @@ class ACN_Data(object):
           date = datetime.strptime(datestr, self.dateformat)
           item[key] = date
 
+  def convert_items(self) -> None:
+    for raw_item in self.raw_items:
+      item = ACN_DataItem()
+
+      if raw_item['doneChargingTime'] is None:
+        item.end = raw_item['disconnectTime']
+      else:
+        item.end = raw_item['doneChargingTime']
+
+      item.kWh = raw_item['kWhDelivered']
+
   def specify_load(self) -> None:
     for item in self.items:
-      if item['doneChargingTime'] is None:
-        item['doneChargingTime'] = item['disconnectTime']
-        
-      chargingTime : timedelta = item['doneChargingTime'] - item['connectionTime']
-      kwH : float = item['kWhDelivered']
-      hours = chargingTime.total_seconds() / 3600
-      kw = kwH * hours
+      charging_period = item.end - item.start
+      item.period_hours = charging_period.total_seconds() / 3600
+      item.kw = item.kWh * item.period_hours
 
-      item['hours'] = hours
-      item['kw'] = kw
+  def process_data(self) -> None:
+    self.parse_dates()
+    self.convert_items()
+    self.specify_load()
 
-  def get_load_of_timeframe(self, start: datetime, end : datetime, period_minutes=10) -> pd.DataFrame:
-    demand_data = DemandData(start, end, period_minutes)
+  def get_load_of_timeframe(self, start: datetime, end: datetime, interval_minutes=10) -> pd.DataFrame:
+    demand_data = DemandData(start, end, interval_minutes)
     
     # todo : fill dataframe
     df = demand_data.data
-    print(df)
 
     return demand_data.data
 
@@ -63,10 +73,7 @@ class ACN_Data(object):
 if __name__ == "__main__":
   data_file_name = "ACN_caltech_2020-10.json"
   ev_data = ACN_Data(data_file_name)
+  ev_data.process_data()
 
-  ev_data.remove_unnescessary_info()
-  ev_data.parse_dates()
-  ev_data.specify_load()
-
-  load_df = ev_data.get_load_of_timeframe(datetime(2020, 1, 1, 0, 0), datetime(2020, 1, 2, 0, 0))
+  load_df = ev_data.get_load_of_timeframe(datetime(2020, 1, 1, 0, 0), datetime(2020, 1, 9, 0, 0))
   print(load_df)
