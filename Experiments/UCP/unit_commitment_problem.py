22a0e79561bc394dc62260b594a77913b25cd444
Fabian Weik <fabian.weik@protonmail.com>
2020-10-26 15:02:01 +0100

    ucp | make UCP instance storable

--- Experiments/UCP/unit_commitment_problem.py
+++ Experiments/UCP/unit_commitment_problem.py
@@ -1,6 +1,7 @@
 #/bin/python3.8
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
+import json
 from typing import List
 
 @dataclass
@@ -8,6 +9,8 @@ class CombustionPlant(object):
   A: float
   B: float
   C: float
+  Pmin: float
+  Pmax: float
   
 
 @dataclass
@@ -15,6 +18,29 @@ class UCP(object):
   load: float
   plants: List[CombustionPlant] = field(default_factory=list)
 
+  def save_to(self, filename) -> None:
+    with open(filename, 'w') as file:
+      json.dump(asdict(self), file, ensure_ascii=False, indent=4)
+
+  def load_from(filename):
+    with open(filename, 'r') as file:
+      instance_dict: dict = json.load(file)
+
+      load: int = instance_dict['load']
+
+      plants_dict_list: List[dict] = instance_dict['plants']
+      plants: List[CombustionPlant] = []
+
+      for plants_dict in plants_dict_list:
+        A: float = plants_dict['A']
+        B: float = plants_dict['B']
+        C: float = plants_dict['C']
+        Pmin: float = plants_dict['Pmin']
+        Pmax: float = plants_dict['Pmax']
+
+        plants.append(CombustionPlant(A, B, C, Pmin, Pmax))
+
+      return UCP(load, plants)
 
 if __name__ == "__main__":
   pass
