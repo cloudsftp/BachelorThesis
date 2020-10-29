431b5bf1804f8f8f39ecb64909d44f89f2fcec83
Fabian Weik <fabian.weik@protonmail.com>
2020-10-29 20:33:28 +0100

    typing | #24 | fix issue with python test script

--- Experiments/run_python_verification.sh
+++ Experiments/run_python_verification.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+status=0
+
 folders="Classical Data UCP"
 for folder in $folders
 do
   cd $folder
   python -m unittest
+  [ $? -eq 0 ] || status=1
   mypy *.py
+  [ $? -eq 0 ] || status=1
   cd ..
 done
+
+exit $status
