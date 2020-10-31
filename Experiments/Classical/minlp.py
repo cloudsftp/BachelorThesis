623f23550a8cddcb6e56ea07018b071145301bef
Fabian Weik <fabian.weik@protonmail.com>
2020-10-31 16:05:59 +0100

    minlp | #23 | finish building minlp from ucp - added constraints

--- Experiments/Classical/minlp.py
+++ Experiments/Classical/minlp.py
@@ -1,9 +1,14 @@
 #!/bin/python3.8
 
+from sys import platform
 from pyomo.core.base.PyomoModel import ConcreteModel # type: ignore
-from pyomo.core.base.objective import Objective # type: ignore
+from pyomo.core.base.constraint import Constraint, ConstraintList
+from pyomo.core.base.expression import Expression # type: ignore
+from pyomo.core.base.objective import Objective
+from pyomo.core.base.plugin import ExpressionFactory # type: ignore
 from pyomo.core.base.set import RangeSet # type: ignore
-from pyomo.core.base.var import Var, VarList # type: ignore
+from pyomo.core.base.var import Var, VarList
+from pyomo.core.expr.logical_expr import inequality # type: ignore
 from pyomo.environ import NonNegativeReals, Boolean # type: ignore
 
 from UCP.unit_commitment_problem import CombustionPlant, UCP, UCP_Solution
@@ -33,15 +38,31 @@ class UCP_MINLP(object):
 
     self.model.o = Objective(rule=objective_function)
 
+  def build_load_constraints(self, ucp: UCP) -> None:
+    def load_constraint_rule(model, t):
+      return ucp.loads[t] <= sum(model.u[(i, t)] * model.p[(i, t)] for i in model.I)
+
+    self.model.l_constr = Constraint(self.model.T, rule=load_constraint_rule)
+
+  def build_power_constraints(self, ucp: UCP) -> None:
+    def power_constraint_rule(model, i, t) -> bool:
+      return inequality(ucp.plants[i].Pmin, model.p[(i, t)], ucp.plants[i].Pmax)
+
+    self.model.p_constr = Constraint(self.model.I, self.model.T, rule=power_constraint_rule)
+
   def __init__(self, ucp: UCP) -> None:
     ''' Build self.model from UCP '''
     self.model = ConcreteModel()
 
-    self.model.I = RangeSet(0, len(ucp.plants) - 1)
-    self.model.T = RangeSet(0, len(ucp.loads) - 1)
+    self.model.I = range(len(ucp.plants))
+    self.model.T = range(len(ucp.loads))
 
     self.instanciate_variables()
     self.build_objective(ucp)
+    self.build_load_constraints(ucp)
+    self.build_power_constraints(ucp)
+
+    self.model.display()
 
   def optimize(self) -> UCP_Solution:
     ''' Optimize self.model and return the solution '''
