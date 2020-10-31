aec57ab8bb2a67df346cc36239e5673ef4d070aa
Fabian Weik <fabian.weik@protonmail.com>
2020-10-31 19:49:21 +0100

    minlp | fix bug

--- Experiments/Classical/minlp.py
+++ Experiments/Classical/minlp.py
@@ -22,43 +22,48 @@ class UCP_MINLP(object):
     self.model.u = create_variable_list(Boolean, 1, self.model.I, self.model.T)
     self.model.p = create_variable_list(NonNegativeReals, 1, self.model.I, self.model.T)
 
-  def build_objective(self, ucp: UCP) -> None:
+  def build_objective(self) -> None:
     def objective_function(model: ConcreteModel) -> float:
       return sum(
         sum(
           model.u[(i, t)] * (
-            ucp.plants[i].A +
-            ucp.plants[i].B * model.p[(i, t)] +
-            ucp.plants[i].C * model.p[(i, t)] ** 2
+            self.ucp.plants[i].A +
+            self.ucp.plants[i].B * model.p[(i, t)] +
+            self.ucp.plants[i].C * model.p[(i, t)] ** 2
           ) for t in self.model.T
         ) for i in self.model.I
       )
 
     self.model.o = Objective(rule=objective_function)
 
-  def build_load_constraints(self, ucp: UCP) -> None:
+  def build_load_constraints(self) -> None:
     def load_constraint_rule(model, t):
-      return ucp.loads[t] <= sum(model.u[(i, t)] * model.p[(i, t)] for i in model.I)
+      return self.ucp.loads[t] <= sum(model.u[(i, t)] * model.p[(i, t)] for i in model.I)
 
     self.model.l_constr = Constraint(self.model.T, rule=load_constraint_rule)
 
-  def build_power_constraints(self, ucp: UCP) -> None:
+  def build_power_constraints(self) -> None:
     def power_constraint_rule(model, i, t) -> bool:
-      return inequality(ucp.plants[i].Pmin, model.p[(i, t)], ucp.plants[i].Pmax)
+      return inequality(
+        self.ucp.plants[i].Pmin,
+        model.p[(i, t)],
+        self.ucp.plants[i].Pmax
+      )
 
     self.model.p_constr = Constraint(self.model.I, self.model.T, rule=power_constraint_rule)
 
   def __init__(self, ucp: UCP) -> None:
     ''' Build self.model from UCP '''
+    self.ucp = ucp
     self.model = ConcreteModel()
 
     self.model.I = range(len(ucp.plants))
     self.model.T = range(len(ucp.loads))
 
     self.instanciate_variables()
-    self.build_objective(ucp)
-    self.build_load_constraints(ucp)
-    self.build_power_constraints(ucp)
+    self.build_objective()
+    self.build_load_constraints()
+    self.build_power_constraints()
 
   def optimize(self) -> UCP_Solution:
     ''' Optimize self.model and return the solution '''
