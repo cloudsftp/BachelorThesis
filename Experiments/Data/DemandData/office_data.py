f08d6f4ea75ab7280accdb04d12f684c767a95dd
Fabian Weik <fabian.weik@protonmail.com>
2020-11-22 17:19:07 +0100

    data | read all office devices data

--- Experiments/Data/DemandData/office_data.py
+++ Experiments/Data/DemandData/office_data.py
@@ -1,26 +1,65 @@
 #!/bin/python3.8
 
 
-import pandas as pd
+import os
+from typing import List
+from matplotlib.pyplot import fill
+import numpy as np # type: ignore
+import pandas as pd # type: ignore
+import matplotlib.pyplot as plt
+from Data.DemandData.demand_data import DemandData
 
-from Data.DemandData.demand_data import DemandData # type: ignore
+
+path_to_data: str = 'Data/DemandData/OfficeData/'
+num_data_items: int = 24 * 6
 
 
-def read_file(name: str) -> DemandData:
-  df: pd.DataFrame = pd.read_csv(name)
-  df = df.drop(columns=['timestamp', 'deviceMac'])
-  df = df.rename(columns={'watt': 'power_kW'})
-  df /= 1000
+def read_file(file_name: str) -> DemandData:
+  device_day_load_df: pd.DataFrame = pd.read_csv(file_name)
+  device_day_load_df = device_day_load_df.drop(columns=['timestamp', 'deviceMac'])
 
+  # convert to kW
+  device_day_load_df = device_day_load_df.rename(columns={'watt': 'power_kW'})
+  device_day_load_df /= 1000
+
+  # convert to 10 minute time resolution
   ten_seconds_in_ten_minutes: int = 60
-  df = df.groupby(df.index // ten_seconds_in_ten_minutes).sum()
+  device_day_load_df = device_day_load_df.groupby(device_day_load_df.index // ten_seconds_in_ten_minutes).sum() / ten_seconds_in_ten_minutes
 
-  return df
+  return device_day_load_df
+
+
+def read_device(device: str) -> pd.DataFrame:
+  print('Reading {}...'.format(device))
+
+  path_to_device: str = os.path.join(path_to_data, device)
+  file_names: List[str] = os.listdir(path_to_device)
+
+  device_load_df: pd.DataFrame = pd.DataFrame(data=np.zeros([num_data_items, 1]), columns=['power_kW'])
+  num_days: int = 0
+
+  for file_name in file_names:
+    device_day_load_df: pd.DataFrame = read_file(os.path.join(path_to_device, file_name))
+    device_load_df = device_load_df.add(device_day_load_df, fill_value=0)
+    num_days += 1
+
+  device_load_df /= num_days
+
+  return device_load_df
 
 
 def read_all():
-  df: pd.DataFrame = read_file('Data/DemandData/OfficeData/CoffeeMaker/2016-02-03.csv')
-  print(df)
+  devices: List[str] = os.listdir(path_to_data)
+
+  load_df: pd.DataFrame = pd.DataFrame(data=np.zeros([num_data_items, 1]), columns=['power_kW'])
+
+  for device in devices:
+    device_load_df: pd.DataFrame = read_device(device)
+    load_df = load_df.add(device_load_df, fill_value=0)
+
+  load_df.plot()
+  plt.show()
+
 
 if __name__ == "__main__":
   read_all()
