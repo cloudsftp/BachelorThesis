f8ece20b71545f2f9398831950711256c7210654
Fabian Weik <fabian.weik@protonmail.com>
2020-12-21 11:54:37 +0100

    centos | add option to choose node

--- Experiments/start_experiment_process.sh
+++ Experiments/start_experiment_process.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+node="0"
+
+if [ "$1" = "--node" ]; then
+  cpu="$2"
+  shift
+  shift
+fi
+
 output_file=experiment_output.log
-numactl python -m Classical.perform_experiments $@ > $output_file &
+numactl --physcpubind=$node --membind=$node \
+  python -m Classical.perform_experiments $@ > $output_file &
