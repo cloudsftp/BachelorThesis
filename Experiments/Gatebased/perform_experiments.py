a051d2ce5e599f3fb43c5655c309b1933ba81629
Fabian Weik <fabian.weik@protonmail.com>
2021-01-27 18:15:22 +0100

    gatebased | clean up

--- Experiments/Gatebased/perform_experiments.py
+++ Experiments/Gatebased/perform_experiments.py
@@ -1,37 +1,29 @@
 #!/bin/python3.8
 
 from qiskit import IBMQ # type: ignore
-from qiskit.aqua import aqua_globals # type: ignore
-from qiskit.aqua.quantum_instance import QuantumInstance # type: ignore
-from qiskit import BasicAer # type: ignore
-from qiskit.circuit.quantumcircuit import QuantumCircuit # type: ignore
-from qiskit.aqua.algorithms import QAOA # type: ignore
-from qiskit.optimization.algorithms import MinimumEigenOptimizer, RecursiveMinimumEigenOptimizer, GroverOptimizer # type: ignore
-from qiskit.optimization.converters import QuadraticProgramToQubo
+from qiskit.optimization.algorithms import GroverOptimizer # type: ignore
+
 
 from UCP.unit_commitment_problem import UCP, ExperimentParameters, CombustionPlant, UCPSolution
+from Data.build_ucp import build_ucp
 from Gatebased.qubo import UCP_QUBO
 
 
-ucp = UCP(ExperimentParameters(2, 2),
-  [40, 50],
-  [
-    CombustionPlant(1, 1, 1, 10, 30, 1, 0),
-    CombustionPlant(1, 1, 1, 10, 30, 0, 2)
-  ]
-)
 
-qubo = UCP_QUBO(ucp)
-qubo.model.prettyprint()
+def grover_opt(qubo: UCP_QUBO, backend) -> None:
+  grover_optimizer = GroverOptimizer(1, num_iterations=5, quantum_instance=backend)
 
-IBMQ.load_account()
-provider = IBMQ.get_provider(group='open', project='main')
-backend = provider.get_backend('ibmq_qasm_simulator')
+  sol: UCPSolution = qubo.optimize(grover_optimizer)
+  print(sol)
 
-qaoa_mes = QAOA(quantum_instance=backend, initial_point=[0., 0.])
-qaoa = MinimumEigenOptimizer(qaoa_mes)
-rqaoa = RecursiveMinimumEigenOptimizer(min_eigen_optimizer=qaoa, min_num_vars=1, min_num_vars_optimizer=qaoa)
+if __name__ == "__main__":
+  param: ExperimentParameters = ExperimentParameters(2, 4)
+  ucp: UCP = build_ucp(param)
 
-grover_optimizer = GroverOptimizer(1, num_iterations=3, quantum_instance=backend)
-sol: UCPSolution = qubo.optimize(grover_optimizer)
-print(sol)
+  qubo = UCP_QUBO(ucp)
+
+  IBMQ.load_account()
+  provider = IBMQ.get_provider(group='open', project='main')
+  backend = provider.get_backend('ibmq_qasm_simulator')
+
+  grover_opt(qubo, backend)
