d23a3c1a7a32c78ba8b9fba0f16d72a7eca7a5ca
Fabian Weik <fabian.weik@protonmail.com>
2021-04-11 10:35:50 +0200

    comments | add comments to Util module

--- Experiments/Util/json_file_handler.py
+++ Experiments/Util/json_file_handler.py
@@ -8,28 +8,36 @@ from typing import Any, Dict
 # NOTE: Functions only work on/with dataclasses
 #       static type checking is not supported
 
-'''
-writes a dataclass to a file
-'''
 def write_dataclass_to(data: Any, file_name: str) -> None:
+  '''
+  writes a dataclass to a file
+
+  :data: data to write
+  :file_name: file to write to
+  '''
   with open(file_name, 'w') as file:
     json.dump(asdict(data), file, ensure_ascii=False, indent=4)
 
-
-'''
-converts a disctinionary to a dataclass of the specified type
-'''
 def convert_dict_to_datclass(dict: Dict, Dataclass) -> Any:
+  '''
+  converts a dictinionary to a dataclass
+
+  :dict: data to convert
+  :Dataclass: type of dataclass
+  '''
   for key in dict:
     if isinstance(dict[key], Dict) and not Dataclass.__annotations__[key] == dict:
       dict[key] = convert_dict_to_datclass(dict[key], Dataclass.__annotations__[key])
 
   return Dataclass(**dict)
 
-'''
-reads a dataclass of the specified type from a file
-'''
 def read_dataclass_from(file_name: str, Dataclass) -> Any:
+  '''
+  reads a dataclass from a file
+
+  :file_name: file to read from
+  :Dataclass: type of dataclass
+  '''
   data: Dict = {}
 
   with open(file_name, 'r') as file:
--- Experiments/Util/logging.py
+++ Experiments/Util/logging.py
@@ -5,12 +5,26 @@ import logging
 from datetime import datetime
 from typing import Any
 
+
 def get_time() -> str:
+  '''
+  returns a string of the current time
+  '''
   return datetime.now().strftime('%m.%d %H:%M:%S')
 
 def debug_msg(msg: Any) -> None:
+  '''
+  prints a debugging message
+
+  :msg: message to print
+  '''
   logging.getLogger().debug(msg)
   print(msg)
 
 def debug_msg_time(msg: Any) -> None:
+  '''
+  prints a debugging message including the current time
+
+  :msg: message to print
+  '''
   debug_msg('[ {} ] . {}'.format(get_time(), msg))
